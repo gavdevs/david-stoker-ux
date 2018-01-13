079ab28ce4dbbed136904a4d047ca926380c6e85
Gav <gavdevs@users.noreply.github.com>
2018-01-12 18:12:45 -0700

    added pages to config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   exportPathMap: function () {
     return {
-      '/': { page: '/' }
-    //   '/about': {page: '/about'},
-    //   '/contact-us/our-location': {page: '/contact-us/our-location'},
-    //   '/terms': {page: '/terms'},
-    //   '/privacy-policy': {page: '/privacy-policy'}
+      '/': { page: '/' },
+      '/contact': {page: '/contact'},
+      '/my-experience': {page: 'my-experience'},
+      '/case-studies': {page: '/case-studies'},
+      '/case-studies/homework': {page: '/case-studies/homework'},
+      '/case-studies/moviepass': {page: '/case-studies/moviepass'},
+      '/case-studies/vmsa': {page: '/case-studies/vmsa'}
     }
   }
 }
