2621ce88dc360cc78bf2d0025cbb6904da32a70a
Gav <gavdevs@users.noreply.github.com>
2018-01-18 19:32:46 -0700

    getting close to having the my-experience page done

--- pages/my-experience.js
+++ pages/my-experience.js
@@ -1,6 +1,6 @@
 import Header from '../components/Header'
 
-import { TimelineContainer, TimeLineSection, TimeLineAside, AsidePlaceholder } from '../styles/pages/my-experience'
+import { TimelineContainer, TimeLineSection, TimeLineAside, AsidePlaceholder, MiddleText } from '../styles/pages/my-experience'
 
 export default () => {
   return (
@@ -16,7 +16,7 @@ export default () => {
           <TimeLineAside>
             <img
               className='top-img'
-              src='/static/my-experience/state-fair-final.png'
+              src='/static/my-experience/degree.png'
               alt='David Stoker UX' />
             <h3>
               A degree in advertising and work as an art director
@@ -34,7 +34,115 @@ export default () => {
               engagement and response across all channels.
             </p>
           </TimeLineAside>
+          <MiddleText>
+            <p>RESEARCHING</p>
+            <p>EMPATHY</p>
+            <p>IDEATING</p>
+            <p>PRESENTING</p>
+          </MiddleText>
+          <AsidePlaceholder />
+        </TimeLineSection>
 
+        <TimeLineSection>
+          <AsidePlaceholder />
+          <MiddleText className='middle-text'>
+            <p>INTERVIEWING</p>
+            <p>STORYTELLING</p>
+          </MiddleText>
+          <TimeLineAside rightAside>
+            <img
+              className='top-img'
+              src='/static/my-experience/videography.png'
+              alt='David Stoker UX' />
+            <h3>
+              Videography work
+            </h3>
+            <p>
+              When I’m interviewing real people featured in a video they’re
+              often unsure and nervous. I try to set them at ease and guide
+              them to offer personal and authentic responses.
+            </p>
+          </TimeLineAside>
+        </TimeLineSection>
+
+        <TimeLineSection>
+          <TimeLineAside>
+            <img
+              className='top-img'
+              src='/static/my-experience/photographer.png'
+              alt='David Stoker UX' />
+            <h3>
+              Commercial photographer
+            </h3>
+            <p>
+              Directing a shoot is a mix of calling the shots, and listening to
+              and respecting other team members who bring their unique
+              abilities. Plus, there’s always, always something that goes wrong
+              - so being quick on my feet and getting help from others is vital.
+            </p>
+            <p>
+              I worked with CompHealth to shoot new photography for a 2014
+              rebrand. I directed and worked with a team of 15, with over 40
+              talent for a week-long shoot. The rebrand was named best new
+              brand by the Utah Marketers Association.
+            </p>
+          </TimeLineAside>
+          <MiddleText>
+            <p>TEAMWORK</p>
+            <p>COMMUNICATION</p>
+            <p>PROBLEM SOLVING</p>
+            <p>PROJECT MANAGEMENT</p>
+          </MiddleText>
+          <AsidePlaceholder />
+        </TimeLineSection>
+
+        <TimeLineSection>
+          <AsidePlaceholder />
+          <MiddleText>
+            <p>VISUAL DESIGN</p>
+            <p>GRID SYSTEMS</p>
+            <p>STYLE GUIDES</p>
+          </MiddleText>
+          <TimeLineAside rightAside>
+            <img
+              className='top-img'
+              src='/static/my-experience/graphic-designer.png'
+              alt='David Stoker UX' />
+            <h3>
+              Graphic designer
+            </h3>
+            <p>
+              My work as the designer at Utah Business magazine taught me the
+              importance of grid systems and consistency. The viewer needs to
+              know how to navigate a web site as well as a magazine, and be
+              able to easily find the information they need.
+            </p>
+          </TimeLineAside>
+        </TimeLineSection>
+
+        <TimeLineSection>
+          <TimeLineAside>
+            <img
+              className='top-img'
+              src='/static/my-experience/music.png'
+              alt='David Stoker UX' />
+            <h3>
+              Composed, recorded and produced an album
+            </h3>
+            <p>
+              Something I always wanted to do. I gathered a handful of musicians
+               and recorded an album of original pieces and arrangements. Then
+               designed album artwork, promotional mailers, performed at arts
+               festivals, self-produced a concert, and even got some press.
+            </p>
+            <p>
+              Checked one off the bucket list!
+            </p>
+          </TimeLineAside>
+          <MiddleText>
+            <p>COLLABORATION</p>
+            <p>RESOURCEFULNESS</p>
+          </MiddleText>
           <AsidePlaceholder />
         </TimeLineSection>
       </TimelineContainer>
--- styles/pages/my-experience.js
+++ styles/pages/my-experience.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
 
-import { darkGray } from '../tools/colors'
+import { darkGray, yellow } from '../tools/colors'
 import { flexContainer, flexItem } from '../tools/mixins'
 
 export const TimelineContainer = styled.div`
   position: relative;
   width: 100%;
-  height: 1000px;
   margin-top: 160px;
   background-color: #fff;
 
@@ -42,15 +41,21 @@ export const TimelineContainer = styled.div`
 `
 export const TimeLineSection = styled.section`
   ${flexContainer()}
-  padding: 40px calc(50% - 600px);
+  padding: 80px calc(50% - 612px);
+  position: relative;
 `
 export const TimeLineAside = styled.aside`
   ${flexItem('200px')};
-  margin: 0 120px;
+  margin: 0 80px;
+  ${props => props.rightAside ? 'margin-left: 160px;' : 'margin-right: 160px;'}
   position: relative;
+  z-index: 3;
 
   > p {
     padding-bottom: 20px;
+    &:last-child {
+      padding-bottom: 0;
+    }
   }
 
   > .top-img {
@@ -58,8 +63,55 @@ export const TimeLineAside = styled.aside`
     top: -160px;
     left: calc(50% - 62.5px)
   }
+
+  > .side-img {
+    position: absolute;
+    bottom: 0;
+    left: - 40px;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: -10%;
+    ${props => props.rightAside ? 'left: -40px;' : 'right: -30px;'}
+    width: 20px;
+    height: 120%;
+    background-color: ${yellow};
+    ${props => props.rightAside ? 'border-radius: 10px 0 0 10px;' : 'border-radius: 0 10px 10px 0;'}
+  }
+
+  :after {
+    content: '';
+    position: absolute;
+    top: calc(-10% + 2px);
+    ${props => props.rightAside ? 'left: -38px;' : 'right: -28px;'}
+    width: 20px;
+    height: calc(120% - 4px);
+    background-color: #fff;
+    ${props => props.rightAside ? 'border-radius: 8px 0 0 8px;' : 'border-radius: 0 8px 8px 0;'}
+  }
 `
 export const AsidePlaceholder = styled.aside`
   ${flexItem('200px')}
-  margin: 0 120px;
+  margin: 0 160px;
+`
+
+export const MiddleText = styled.div`
+  ${flexContainer('flex-start', 'center', 'space-around')}
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 80px 0;
+  z-index: 2;
+  text-align: center;
+
+  > p {
+    width: 100%;
+    padding: 4px;
+    background-color: #fff;
+    color: ${yellow};
+  }
 `
